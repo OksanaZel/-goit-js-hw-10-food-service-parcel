015a7e71584c9adf9025015e6b563b7b445150c6
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-04-26 18:45:26 +0300

    Добавляет решение

---
+++ src/js/toggle-theme.js
@@ -0,0 +1,38 @@
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const USER_THEME_KEY = 'my-theme';
+
+const themeSwitchToggle = document.querySelector('#theme-switch-toggle');
+
+document.body.classList.add(Theme.LIGHT);
+
+userLocalStorageCheck();
+
+themeSwitchToggle.addEventListener('change', onChangeTheme);
+
+function onChangeTheme(evt) {
+        if (evt.target.checked) {
+        document.body.classList.add(Theme.DARK);
+        document.body.classList.remove(Theme.LIGHT);
+        localStorage.setItem(USER_THEME_KEY, Theme.DARK);
+} else {
+        document.body.classList.add(Theme.LIGHT);
+        document.body.classList.remove(Theme.DARK);
+                
+        localStorage.setItem(USER_THEME_KEY, Theme.LIGHT);
+}
+}
+
+function userLocalStorageCheck() {
+        const userThemeCheck = localStorage.getItem(USER_THEME_KEY);
+        if (userThemeCheck) {
+                document.body.classList.add(userThemeCheck);
+        }
+
+        if (userThemeCheck === Theme.DARK) {
+                themeSwitchToggle.checked = true;
+        }
+}
